e80513a43a9f2a1d500488e9a1a4396e02187471
dhwang21 <hwang.derrick@gmail.com>
2018-09-19 13:21:58 -0700

    submittig with work of yelp feature and map rework

--- server.js
+++ server.js
@@ -47,36 +47,31 @@ function Weather(day){
   this.forecast = day.summary;
 }
 
-function getYelp(request, response){
-  const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
+// function getYelp(request, response){
+//   const url = `https://api.yelp.com/v3/businesses/search?location=${request.query.data.search_query}`;
 
-  superagent.get(url).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+//   superagent.get(url).set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
 
-    .then(result =>{
-      const yelpSummaries = [];
+//     .then(result =>{
+//       const yelpSummaries = [];
 
-      result.body.forEach( result => {
-        const summary = new Yelp(result);
-        yelpSummaries.push(summary);
-      });
-      console.log('Yelp result.body: ', result.body.businesses);
-      response.send(yelpSummaries);
-    });
-}
+//       result.body.forEach( result => {
+//         const summary = new Yelp(result);
+//         yelpSummaries.push(summary);
+//       });
+//       console.log('Yelp result.body: ', result.body.businesses);
+//       response.send(yelpSummaries);
+//     });
+// }
 
 
-function Weather(day){
-  this.time = new Date(day.time * 1000).toString().slice(0, 15);
-  this.forecast = day.summary;
-}
-
-function Yelp(result){
-  this.name = result.body.businesses.name;
-  this.image_url = result.body.businesses.image_url;
-  this.price = result.body.businesses.price;
-  this.rating = result.body.businesses.rating;
-  this.url = result.body.businesses.url;
-}
+// function Yelp(result){
+//   this.name = result.body.businesses.name;
+//   this.image_url = result.body.businesses.image_url;
+//   this.price = result.body.businesses.price;
+//   this.rating = result.body.businesses.rating;
+//   this.url = result.body.businesses.url;
+// }
 
 
 
@@ -86,8 +81,6 @@ function Yelp(result){
 
 
 
-
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
